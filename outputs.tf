8e035bab92bafd7ef10bdb211f715bd175c5f171
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "data_factory_linked_service_mysqls" {
-  description = "All data_factory_linked_service_mysql resources"
-  value       = azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls
-}
 output "data_factory_linked_service_mysqls_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.additional_properties }
 }
 output "data_factory_linked_service_mysqls_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.annotations }
 }
 output "data_factory_linked_service_mysqls_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.connection_string }
 }
 output "data_factory_linked_service_mysqls_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.data_factory_id }
 }
 output "data_factory_linked_service_mysqls_description" {
-  description = "List of description values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.description]
+  description = "Map of description values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.description }
 }
 output "data_factory_linked_service_mysqls_driver_version" {
-  description = "List of driver_version values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.driver_version]
+  description = "Map of driver_version values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.driver_version }
 }
 output "data_factory_linked_service_mysqls_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_mysqls_name" {
-  description = "List of name values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.name]
+  description = "Map of name values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.name }
 }
 output "data_factory_linked_service_mysqls_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_mysqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.parameters }
 }
 
