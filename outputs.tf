00dbfc5dea495ae022b8932746e71547be3f6884
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factory_linked_service_mysqls_id" {
+  description = "Map of id values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.id }
+}
 output "data_factory_linked_service_mysqls_additional_properties" {
   description = "Map of additional_properties values across all data_factory_linked_service_mysqls, keyed the same as var.data_factory_linked_service_mysqls"
   value       = { for k, v in azurerm_data_factory_linked_service_mysql.data_factory_linked_service_mysqls : k => v.additional_properties }
